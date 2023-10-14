c48d6cb5f2ab77ceca797f3b4cca267b98e548d4
Philip Wolfe <61720289+philipwolfe@users.noreply.github.com>
2023-10-13 23:41:30 -0500

    Initial commit
    Initial commit

---
+++ scripts/dhtml.js
@@ -0,0 +1,28 @@
+﻿var $ = function (id) { return document.getElementById(id); };
+
+
+function getMouseCoords(ev)
+{
+	if (ev.pageX || ev.pageY)
+	{
+		return { x: ev.pageX, y: ev.pageY };
+	}
+	return {
+		x: ev.clientX + document.body.scrollLeft - document.body.clientLeft,
+		y: ev.clientY + document.body.scrollTop - document.body.clientTop
+	};
+}
+
+function getMouseOffset(target, ev)
+{
+	var docPos = getPosition(target);
+	var mousePos = getMouseCoords(ev);
+	return { x: mousePos.x - docPos.x, y: mousePos.y - docPos.y };
+}
+
+function getPosition(el)
+{
+	var clientRect = el.getBoundingClientRect();
+
+	return { x: Math.round(clientRect.left), y: Math.round(clientRect.top) };
+}
